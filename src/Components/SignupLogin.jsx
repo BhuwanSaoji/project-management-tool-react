d9dc01e1f639e24cd6b01101e056243ddf80788e
Bhuwan Saoji <bhuwan.saoji@compport.com>
2023-01-29 23:13:01 +0530

    sign up structure completed

--- src/Components/SignupLogin.jsx
+++ src/Components/SignupLogin.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import './SignupLogin.css'
+import {CiMail} from 'react-icons/ci';
 const SignupLogin = () => {
   return (
     <div className='main_signup_container'>
-    <div className='sign_up_card'>
-    <div className='details_signup'>
-
-    </div>
-    <div className='background_signup'>
-    <h1 className='background_heading'>Hello, Friend!!</h1>
-    <p className='background_para'>Enter your details</p>
-    <p className='background_para'>and start a journey with us</p>
-    <button className='btn btn-signup'>Sign Up</button>
-
-    </div>
-
-    </div>
-
+      <div className='sign_up_card'>
+        <div className='details_signup'>
+            <h2 className='signup__header'>Sign in to Productivitie</h2>
+            <input type="email" name="email" placeholder='Email' className='mail'/>
+            <input type="password" name="password" placeholder='Password' />
+            <small>Forgot your password?</small>
+            <button className='btn btn-log-in'>Sign in</button>
+        </div>
+        <div className='background_signup'>
+          <h1 className='background_heading'>Hello, Friend!!</h1>
+          <p className='background_para'>Enter your details</p>
+          <p className='background_para'>and start a journey with us</p>
+          <button className='btn btn-signup'>Sign Up</button>
+        </div>
+      </div>
     </div>
     
   )
